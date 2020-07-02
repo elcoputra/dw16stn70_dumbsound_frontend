bd665f888b3e3b7672341bbe1ec696eceb85f3a6
Elcoputra <elcoputra94@gmail.com>
2020-07-02 19:15:52 +0700

    litle update

--- src/components/musicPlayer.js
+++ src/components/musicPlayer.js
@@ -1,33 +1,11 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
-import { AppBar, Toolbar, Avatar, Grid, Typography } from '@material-ui/core';
-import { compose } from 'recompose';
 import { connect } from 'react-redux';
 import { clearPlaylist } from '../redux/actions/song_actions';
 import { getArtistBySongAction } from '../redux/actions/artist_action';
 import ReactJkMusicPlayer from 'react-jinke-music-player';
 import 'react-jinke-music-player/assets/index.css';
-
-const styles = (theme) => ({
-  appBar: {
-    width: '100%',
-    height: 85,
-    backgroundColor: '#2e2d2d',
-    position: 'fixed',
-    top: 'auto',
-    bottom: 0,
-  },
-  player: {
-    // marginTop: 42,
-  },
-  Avatar: {
-    width: '50px',
-    height: '50px',
-  },
-  judul: {
-    marginRight: 1088,
-  },
-});
+import './musicPlayer.css';
 
 class musicPlayer extends Component {
   constructor(props) {
@@ -53,7 +31,6 @@ class musicPlayer extends Component {
     this.props.getArtistBySongAction(audioInfo.name);
   };
   render() {
-    const { classes } = this.props;
     const { song } = this.props.getDetailSongReducer;
     const { userState } = this.props.authReducer;
     return (
@@ -91,4 +68,4 @@ const mapStateToProps = (state) => {
     authReducer: state.authReducer,
   };
 };
-export default compose(withStyles(styles), connect(mapStateToProps, { clearPlaylist, getArtistBySongAction }))(musicPlayer);
+export default connect(mapStateToProps, { clearPlaylist, getArtistBySongAction })(musicPlayer);
