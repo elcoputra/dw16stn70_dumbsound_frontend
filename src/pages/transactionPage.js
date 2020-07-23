882ff82a8d052d783827e770b9ef4e3810aa8ac4
Elcoputra <elcoputra94@gmail.com>
2020-07-23 07:28:53 +0700

    ngerapihin

--- src/pages/transactionPage.js
+++ src/pages/transactionPage.js
@@ -1,15 +1,7 @@
 import React, { Component } from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, Grid, Button } from '@material-ui/core';
 import ArrowDropDownIcon from '@material-ui/icons/ArrowDropDown';
 // moment adalah modul mengenawi waktu
 import Moment from 'moment';
